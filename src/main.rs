986f9437105b8e4d71f2cefbd43fe591f45155a8
fa1ry <mrp2box@disroot.org>
2023-11-05 14:05:59 +0000

    make main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::io;
+use std::process;
+
+fn checkProcessID() {
+   let currentProcessID = process::id();
+   println!("...current process is: {}", currentProcessID);
+   if currentProcessID == 1 {
+      println!("rusty is on PID 1!");
+   } else {
+     println!("rusty is not running on PID 1...... continuing for now....");
+   }
+}
+
+fn importConfiguration() {
+   println!("Do Later");
+}
+
+fn importRC() {
+   println!("also do later");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("...starting rusty");
+    checkProcessID();
 }
